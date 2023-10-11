c002643ae3616ccf05e165b1eb9e4c32e1083b3a
Tianxiang Ren <tianxia3@ualberta.ca>
2023-10-11 13:49:20 -0600

    fix: use application/x-www-form-urlencoded

--- httpclient.py
+++ httpclient.py
@@ -23,6 +23,7 @@ import socket
 import re
 # you may use urllib to encode data appropriately
 import urllib.parse
+import json
 
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
@@ -39,23 +40,16 @@ class ParsedHttpResponse:
         self.body = body
 
 class HTTPClient(object):
-
-
-    # 3xx codes that has redirections
-    # https://stackoverflow.com/questions/16194988/for-which-3xx-http-codes-is-the-location-header-mandatory
-    redirection_codes = set([301, 302, 303, 307, 308])
     def get_host_port(self,url):
         parse_result = urllib.parse.urlparse(url)
         return [parse_result.hostname, parse_result.port]
 
-    def connect(self, host, port) -> socket.socket:
-        s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-        s.connect((host, port))
-        return s
-
     def parse_http_response(self, data) -> ParsedHttpResponse:
         tokens = data.split("\r\n")
-        status_code = int(tokens[0][9: 12])
+        if len(tokens[0]) > 12: 
+            status_code = int(tokens[0][9: 12])
+        else:
+            status_code = 0
         headers = {}
         body = ""
         for i in range(1, len(tokens)):
@@ -73,10 +67,13 @@ class HTTPClient(object):
         if body_start_index != -1:
             body = data[body_start_index + 4:]
         rval = ParsedHttpResponse(status_code, headers, body)
-        return rval        
+        return rval
 
-    # read everything from the socket
-    def recvall(self, sock):
+    def fetch(self, host, port, request) -> str:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((host, port))
+        sock.sendall(request.encode('utf-8'))
+        sock.shutdown(socket.SHUT_WR)
         buffer = bytearray()
         done = False
         while not done:
@@ -85,7 +82,37 @@ class HTTPClient(object):
                 buffer.extend(part)
             else:
                 done = not part
+        sock.close()
         return buffer.decode('utf-8')
+    
+    def build_http_request(self, url, method, args):
+        parse_result = urllib.parse.urlparse(url)
+        host = parse_result.hostname
+        path = parse_result.path
+        if path == "":
+            path = "/"
+        query = parse_result.query
+        headers = {}
+        payload = ""
+
+        if query != "":
+            path += "?" + query
+        if method == "POST":
+            if args is not None:
+                headers["Content-Type"] = "application/x-www-form-urlencoded"
+                payload = urllib.parse.urlencode(args)
+                headers["Content-Length"] = len(payload)
+            else:
+                headers["Content-Length"] = 0
+        request = method + " " + path + " HTTP/1.1\r\nHost: " + host + "\r\n"
+
+        for key, value in headers.items():
+            request += key + ": " + str(value) + "\r\n"
+        
+        request += "\r\n" + payload
+
+        return request
+
 
     def GET(self, url, args=None):
         parse_result = urllib.parse.urlparse(url)
@@ -93,38 +120,30 @@ class HTTPClient(object):
         port = parse_result.port
         if port is None:
             port = 80
-        path = parse_result.path
-        sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-        sock.connect((host, port))
-        request = b"GET / HTTP/1.1\r\nHost:www.google.com\r\n\r\n"
-        sock.sendall(b"GET / HTTP/1.1\r\nHost:www.google.com\r\n\r\n")
-        sock.shutdown(socket.SHUT_WR)
-        buffer = b''
-        while True:
-            part = sock.recv(4096)
-            if part:
-                buffer += part
-            else:
-                break
-        response = buffer.decode('utf-8')
-        print("GET:", response)
+        
+        request = self.build_http_request(url, "GET", args)
+        response = self.fetch(host, port, request)
         parsed_response = self.parse_http_response(response)
-        headers = parsed_response.headers
         code = parsed_response.status_code
         body = parsed_response.body
-        
-        if parsed_response in self.redirection_codes and 'Location' in headers:
-            redirected_location = headers['Location']
-            if isinstance(redirected_location, list):
-                redirected_location = redirected_location[0]
-            return self.GET(redirected_location)
+        print(response)
 
         return HTTPResponse(code, body)
 
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
+        parse_result = urllib.parse.urlparse(url)
+        host = parse_result.hostname
+        port = parse_result.port
+        if port is None:
+            port = 80
+        
+        request = self.build_http_request(url, "POST", args)
+        response = self.fetch(host, port, request)
+        print(response)
+        parsed_response = self.parse_http_response(response)
+        code = parsed_response.status_code
+        body = parsed_response.body
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
@@ -135,12 +154,28 @@ class HTTPClient(object):
     
 if __name__ == "__main__":
     client = HTTPClient()
-    client.GET('http://www.google.com/')
-    command = "GET"
-    if (len(sys.argv) <= 1):
-        help()
-        sys.exit(1)
-    elif (len(sys.argv) == 3):
-        print(client.command( sys.argv[2], sys.argv[1] ))
-    else:
-        print(client.command( sys.argv[1] ))
+    client.GET("http://slashdot.org/")
+    # client.GET("http://www.google.com/")
+    # client.GET("http://c2.com/cgi/wiki?CommonLispHyperSpec")
+    # client.GET("http://[2605:fd00:4:1001:f816:3eff:fec0:41df]/parts/")
+    # args = {
+    #     "id": "a6372d7f-095f-4268-9c5f-9d7d2dbc7de5",
+    #     "name": "Sapphire PULSE Radeon RX 7800 XT 16 GB Video Card",
+    #     "type": "GPU",
+    #     "release_date": 1693958400,
+    #     "core_clock": 1295.0,
+    #     "clock_unit": "MHz",
+    #     "price": 745.9,
+    #     "TDP": 266,
+    #     "part_no": "11330-02-20GGGGGGGG"
+    # }
+    # client.POST("http://127.0.0.1:8000/parts/a6372d7f-095f-4268-9c5f-9d7d2dbc7de5", args)
+    # client.POST('http://www.google.com/')
+    # command = "GET"
+    # if (len(sys.argv) <= 1):
+    #     help()
+    #     sys.exit(1)
+    # elif (len(sys.argv) == 3):
+    #     print(client.command( sys.argv[2], sys.argv[1] ))
+    # else:
+    #     print(client.command( sys.argv[1] ))
